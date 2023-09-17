15831ec386936706d4cf8bee6b889de4a68763ce
ted <tkk21@case.edu>
2023-09-17 18:24:38 -0400

    stream wav

--- api_app.py
+++ api_app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
-import argparse
 import uvicorn
+import argparse
+import struct
 
 
 from tts import tts
@@ -22,12 +23,37 @@ class RvcOptions(BaseModel):
     protect0: float
 
 
-def yield_audio(audio_arr):
-    yield from audio_arr
-
-
 @app.post("/tts")
 def convert_text_to_rvc_speech(options: RvcOptions | None = None):
+    def generate_audio_stream(sample_rate, audio_data):
+        # Convert audio data to bytes
+        audio_bytes = audio_data.tobytes()
+
+        # Calculate the total length of audio data in bytes
+        audio_length = len(audio_bytes)
+
+        # Send the WAV header
+        wav_header = struct.pack(
+            "<4sI4s4sIHHIIHH4sI",
+            b"RIFF",
+            audio_length + 44,
+            b"WAVE",
+            b"fmt ",
+            16,
+            1,
+            1,
+            sample_rate,
+            sample_rate * 2,
+            2,
+            16,
+            b"data",
+            audio_length,
+        )
+        yield wav_header
+
+        # Send the audio data
+        yield audio_bytes
+
     try:
         info_text, edge_tts_output, tts_output = tts(
             options.rvc_model_name,
@@ -42,7 +68,9 @@ def convert_text_to_rvc_speech(options: RvcOptions | None = None):
         print(f"info_text: ${info_text}")
         print(f"edge_tts_output: ${edge_tts_output}")
         print(f"tts_output: ${tts_output}")
-        return StreamingResponse(yield_audio(tts_output[1]), media_type="audio/wav")
+        return StreamingResponse(
+            generate_audio_stream(tts_output[0], tts_output[1]), media_type="audio/wav"
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
